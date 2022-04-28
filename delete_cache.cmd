ddbc68e0cea4d08b694892492a0f413cfb8fce02
Daniele <49653822+danieleongaro@users.noreply.github.com>
2022-04-28 20:40:06 +0200

    Add files via upload

---
+++ delete_cache.cmd
@@ -0,0 +1,22 @@
+@echo Off
+
+:: con /R looppa in tutti i file di tutte le sottocartelle
+echo ---List files .slxc---
+for /R %%G in (*.slxc) do (
+	:: echo %%~nxG
+	echo %%~fG
+	:: senza /P non chiede conferma (exp: del /P)
+	:: Guida dice che con /Q non chiede conferma, nel dubbio uso anche questo
+	del /Q "%%~fG"
+)
+
+:: con /D /R looppa in tutte le cartelle e sottocartelle
+echo ---List folders---
+for /D /R %%G in (*) do (
+	echo %%G
+	if %%~nxG == slprj (
+		echo deleting
+		:: /S cancella anche file in cartella e /Q non chiede conferma
+		rmdir /S /Q "%%~fG"
+	)
+)
